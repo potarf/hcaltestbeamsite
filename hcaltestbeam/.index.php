7142561a24e691ea270770c9ee8154150a03cc7c
potarf <jordan_potarf@baylor.edu>
2017-07-19 14:29:44 +0200

    working on homepage

--- hcaltestbeam/.index.php
+++ hcaltestbeam/.index.php
@@ -11,118 +11,19 @@
 </head>
 
 <body>
-  <div id="container"> 
-    <h1>Directory Contents</h1>
-    <h4><?php readfile(".README.txt"); ?></h4>
-    <table class="sortable">
-      <thead>
-        <tr>
-          <th>Filename</th>
-          <th>Type</th>
-          <th>Size <small>(bytes)</small></th>
-          <th>Date Modified</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php
-        // Opens directory
-        $myDirectory=opendir(".");
+    <div id="container">
+        <h1>Hcal testbeam 2017 (site under construction)</h1>
+
+        <div class="row">
+            <div class="col-md-3">Run Number <small>(detail)</small></div>
+            <div class="col-md-3">Analysis</div>
+            <div class="col-md-3">Status</div>
+            <div class="col-md-3">Date Taken</div>
+        </div>
         
-        // Gets each entry
-        while($entryName=readdir($myDirectory)) {
-          $dirArray[]=$entryName;
-        }
-        
-        // Finds extensions of files
-        function findexts ($filename) {
-          $filename=strtolower($filename);
-          $exts=split("[/\\.]", $filename);
-          $n=count($exts)-1;
-          $exts=$exts[$n];
-          return $exts;
-        }
-        
-        // Closes directory
-        closedir($myDirectory);
-        
-        // Counts elements in array
-        $indexCount=count($dirArray);
-        
-        // Sorts files
-        sort($dirArray);
-        
-        // Loops through the array of files
-        for($index=0; $index < $indexCount; $index++) {
-        
-          if(substr("$dirArray[$index]", 0, 1) != ".") {
-          
-          // Gets File Names
-          $name=$dirArray[$index];
-          $namehref=$dirArray[$index];
-          
-          // Gets Extensions 
-          $extn=findexts($dirArray[$index]); 
-          
-          // Gets file size 
-          $size=number_format(filesize($dirArray[$index]));
-          
-          // Gets Date Modified Data
-          $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-          $timekey=date("YmdHis", filemtime($dirArray[$index]));
-          
-          // Prettifies File Types, add more to suit your needs.
-          switch ($extn){
-            case "png": $extn="PNG Image"; break;
-            case "jpg": $extn="JPEG Image"; break;
-            case "svg": $extn="SVG Image"; break;
-            case "gif": $extn="GIF Image"; break;
-            case "ico": $extn="Windows Icon"; break;
-            
-            case "txt": $extn="Text File"; break;
-            case "log": $extn="Log File"; break;
-            case "htm": $extn="HTML File"; break;
-            case "php": $extn="PHP Script"; break;
-            case "js": $extn="Javascript"; break;
-            case "css": $extn="Stylesheet"; break;
-            case "pdf": $extn="PDF Document"; break;
-            
-            case "zip": $extn="ZIP Archive"; break;
-            case "bak": $extn="Backup File"; break;
-            
-            default: $extn=strtoupper($extn)." File"; break;
-          }
-          
-          // Separates directories
-          if(is_dir($dirArray[$index])) {
-            $extn="&lt;Directory&gt;"; 
-            $size="&lt;Directory&gt;"; 
-            $class="dir";
-          } else {
-            $class="file";
-          }
-          
-          // Cleans up . and .. directories 
-          if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
-          if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-          
-          // Print 'em
-          print("
-          <tr class='$class'>
-            <td><a href='./$namehref'>$name</a></td>
-            <td><a href='./$namehref'>$extn</a></td>
-            <td><a href='./$namehref'>$size</a></td>
-            <td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
-          </tr>");
-          }
-        }
-      ?>
-      </tbody>
-    </table>
-  
-    <h2><a href=".">Reload</a></h2>
-    
-  </div>
-  
+
+    </div>
+
 </body>
 
 </html>
