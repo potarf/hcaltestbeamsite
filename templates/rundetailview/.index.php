4ea9342b2114c74de30296c62a54968158ab6124
Jordan Potarf <jordan_potarf@baylor.edu>
2017-07-20 02:14:28 +0200

    creating run detail view

---
+++ templates/rundetailview/.index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Directory Contents</title>
+  <script src=".sorttable.js"></script>
+
+  <?php readfile(".src/loadflatui_d0.html"); ?>
+  <link rel="stylesheet" href=".src/style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Hcal testbeam 2017</h1>
+        <h2>(site under construction)</h2>
+
+        <div class="row">
+            <div class="col-md-3">Run Number <small>(detail)</small></div>
+            <div class="col-md-3">Analysis</div>
+            <div class="col-md-3">Status</div>
+            <div class="col-md-3">Date Taken</div>
+        </div>
+
+        <?php
+        //format of inputs file:
+        //[Runnumber] [Analysis link] [Status] [Date of run]
+        $runnum="002529"; //from data file
+        $analysisref="."; //from whether analysis files exist
+        $status="Good"; //input by the shifter
+        $datetaken="2017-02-29"; //from data file last modified
+
+
+        $files = array_diff(scandir('./rawdata'), array('.', '..'));
+        for($index=0; $index < 5; $index++) {
+            print("
+            <div class=\"row\">
+                <div class=\"col-md-3\">$runnum<small>(detail)</small></div>
+                <div class=\"col-md-3\">$analysisref</div>
+                <div class=\"col-md-3\">$status</div>
+                <div class=\"col-md-3\">$datetaken</div>
+            </div>");
+        }
+
+
+        ?>
+
+
+    </div>
+
+</body>
+
+</html>
