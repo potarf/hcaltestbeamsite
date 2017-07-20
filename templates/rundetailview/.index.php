d8e35489b56ec5952eed8a73983e3c671c84951b
potarf <jordan_potarf@baylor.edu>
2017-07-20 02:24:31 +0200

    editing files and abusing git

--- templates/rundetailview/.index.php
+++ templates/rundetailview/.index.php
@@ -6,7 +6,7 @@
   <title>Directory Contents</title>
   <script src=".sorttable.js"></script>
 
-  <?php readfile(".src/loadflatui_d0.html"); ?>
+  <?php readfile("../.src/loadflatui_d0.html"); ?>
   <link rel="stylesheet" href=".src/style.css">
 </head>
 
@@ -14,13 +14,30 @@
     <div id="container">
         <h1>Hcal testbeam 2017</h1>
         <h2>(site under construction)</h2>
-
+Beam file	Trigger	Energy	Run key	Number of events	Eta	Table eta	Phi	Table phi	Start time	End time
         <div class="row">
-            <div class="col-md-3">Run Number <small>(detail)</small></div>
-            <div class="col-md-3">Analysis</div>
+            <div class="col-md-6"><h3>[Run Number]</h3></div>
             <div class="col-md-3">Status</div>
             <div class="col-md-3">Date Taken</div>
         </div>
+        <div class="row">
+            <div class="col-md-4">Path to raw data</div>
+            <div class="col-md-4">Path to processed data file</div>
+            <div class="col-md-4">Analysis</div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">Beam file</div>
+            <div class="col-md-1">Trigger Energy</div>
+            <div class="col-md-2">Run key</div>
+            <div class="col-md-1">Number of events</div>
+            <div class="col-md-1">Eta</div>
+            <div class="col-md-1">Table eta</div>
+            <div class="col-md-1">Phi</div>
+            <div class="col-md-1">Table phi</div>
+            <div class="col-md-1">Start time</div>
+            <div class="col-md-1">End time</div>
+        </div>
+
 
         <?php
         //format of inputs file:
